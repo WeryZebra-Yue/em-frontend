1f1db169a7db663c129c4ec600da693800a6005a
Ayush Master <75676675+WeryZebra-Yue@users.noreply.github.com>
2023-03-06 22:33:22 +0530

    Update constants.js

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -2,4 +2,4 @@ const BASE_URL = "https://project-exam-backend-o9od.vercel.app/api/v1";
 
 export const EXAMPLE_URL = `${BASE_URL}/examples`;
 export const ADMIN_URL = `${BASE_URL}/admin`;
-export const UPLOAD_IMAGE_URL = `https://chatappusingsocketio.herokuapp.com/addImages`;
+export const UPLOAD_IMAGE_URL = `https://storage-makeit.vercel.app/addImages`;
